0a49ac3baaacf2a4b5f9dfcac1ecd7e8fc89766c
PNilanga <nilangapraneeth2@gmail.com>
2024-11-17 22:09:55 -0500

    Innitial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.food_delivery_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.food_delivery_system to javafx.fxml;
+    exports com.example.food_delivery_system;
+}
